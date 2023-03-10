d5bb03c9c52e25c2f3e470d3594d1068e9c51de8
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-03-10 10:25:39 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string){
+    return string.toUpperCase()
+}
+
+function whisper(string){
+    return string.toLowerCase()
+}
+function logShout(string){
+    'string'.toUpperCase();
+    const spy='HELLO'
+    console.log('HELLO')
+}
+function logWhisper(string){
+string .toLowerCase()
+const spy='hello'
+console.log('hello')
+}
+function sayHiToHeadphonedRoommate (string){
+
+    if (string .toUpperCase){return "YES INDEED!"}
+    
+    else if (string .toLowerCase){return "I can't hear you!"}
+    
+    else if (string("Let's have dinner together!")){return "I would love to!"}
+}
